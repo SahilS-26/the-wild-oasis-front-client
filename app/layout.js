716da6785042703518586c7ce31a92f65d9dd338
Sahil Suthar <sahil26dd@gmail.com>
2025-05-21 23:12:12 +0530

    styling with tailwind css

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import "@/app/_styles/globals.css";
 import Logo from "@/app/_components/Logo";
 import Navigation from "@/app/_components/Navigation";
 
@@ -9,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="bg-primary-950 text-primary-100 min-h-screen">
         <header>
           <Logo />
           <Navigation />
