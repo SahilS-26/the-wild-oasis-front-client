f29f46384755234b3291e3433f471e3ff2a3ce6d
Sahil Suthar <sahil26dd@gmail.com>
2025-05-22 22:32:24 +0530

    Improved navigation and root layout

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -4,33 +4,35 @@ export default function Navigation() {
   const NAV_LINKS = [
     {
       id: 1,
-      name: "Home",
-      link: "/",
+      name: "Cabins",
+      url: "/cabins",
     },
     {
       id: 2,
-      name: "Cabins",
-      link: "/cabins",
+      name: "About",
+      url: "/about",
     },
     {
       id: 3,
-      name: "About",
-      link: "/about",
-    },
-    {
-      id: 4,
-      name: "Your account",
-      link: "/account",
+      name: "Guest Area",
+      url: "/account",
     },
   ];
 
   return (
-    <ul>
-      {NAV_LINKS.map((nav) => (
-        <li key={nav.id}>
-          <Link href={nav.link}>{nav.name}</Link>
-        </li>
-      ))}
-    </ul>
+    <nav className="z-10 text-xl">
+      <ul className="flex gap-16 items-center">
+        {NAV_LINKS.map((nav) => (
+          <li key={nav.id}>
+            <Link
+              href={nav.url}
+              className="hover:text-accent-400 transition-colors"
+            >
+              {nav.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
   );
 }
--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import "@/app/_styles/globals.css";
 import Logo from "@/app/_components/Logo";
 import Navigation from "@/app/_components/Navigation";
 import { Josefin_Sans } from "next/font/google";
+import Header from "./_components/Header";
 
 const joseFin = Josefin_Sans({
   subsets: ["latin"],
@@ -21,14 +22,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${joseFin.className} bg-primary-950 text-primary-100 min-h-screen`}
+        className={`${joseFin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
       >
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
-        <footer>Copyright &copy; by The Wild Oasis.</footer>
+        <Header />
+
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   );
