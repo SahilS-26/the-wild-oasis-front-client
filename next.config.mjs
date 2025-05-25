378e7e36ff17a9e9504ac1c4764cc3fa4cf5a5ce
Sahil Suthar <sahil26dd@gmail.com>
2025-05-25 16:57:13 +0530

    fetching and displaying cabin list

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    // remotePatterns: [new URL("https://assets.example.com/account123/**")],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "oiacuuealxphliibmqof.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/cabin-images/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
