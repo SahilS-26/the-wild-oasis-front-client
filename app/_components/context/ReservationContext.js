62ccaf98821449cb99cb744765fe3655fe936755
Sahil Suthar <sahil26dd@gmail.com>
2025-06-08 23:27:59 +0530

    using the context for state management

---
+++ app/_components/context/ReservationContext.js
@@ -0,0 +1,34 @@
+"use client";
+
+import { createContext, useContext, useState } from "react";
+
+const ReservationContext = createContext();
+
+const initialState = { from: null, to: null };
+
+function ReservationProvider({ children }) {
+  const [range, setRange] = useState(initialState);
+
+  const resetRange = () => {
+    setRange(initialState);
+  };
+
+  return (
+    <ReservationContext.Provider value={{ range, setRange, resetRange }}>
+      {children}
+    </ReservationContext.Provider>
+  );
+}
+
+function useReservation() {
+  const context = useContext(ReservationContext);
+
+  if (!context)
+    throw new Error(
+      "Reservation context being used outside of reservation context."
+    );
+
+  return context;
+}
+
+export { ReservationProvider, useReservation };
