2e0fff0de62fa2b194ca1ee49522ceca3fa2cf23
Sahil Suthar <sahil26dd@gmail.com>
2025-06-09 23:38:44 +0530

    Setting up NextAuth and getting user session

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -1,35 +1,59 @@
 import Link from "next/link";
+import { auth } from "../_lib/auth";
+import Image from "next/image";
 
-export default function Navigation() {
+export default async function Navigation() {
+  const session = await auth();
+
   const NAV_LINKS = [
     {
       id: 1,
       name: "Cabins",
+      query: "cabins",
       url: "/cabins",
     },
     {
       id: 2,
       name: "About",
+      query: "about",
       url: "/about",
     },
     {
       id: 3,
       name: "Guest Area",
+      query: "account",
       url: "/account",
     },
   ];
+
+  console.log(session);
 
   return (
     <nav className="z-10 text-xl">
       <ul className="flex gap-16 items-center">
         {NAV_LINKS.map((nav) => (
           <li key={nav.id}>
-            <Link
-              href={nav.url}
-              className="hover:text-accent-400 transition-colors"
-            >
-              {nav.name}
-            </Link>
+            {nav.query === "account" && session?.user?.image ? (
+              <Link
+                href={nav.url}
+                className="hover:text-accent-400 transition-colors flex items-center gap-4"
+              >
+                <img
+                  src={session?.user?.image}
+                  alt={session?.user?.name}
+                  referrerPolicy="no-referrer"
+                  className="h-8 rounded-full"
+                />
+                <span>{nav.name}</span>
+              </Link>
+            ) : (
+              <Link
+                href={nav.url}
+                className="hover:text-accent-400 transition-colors"
+              >
+                {nav.name}
+              </Link>
+            )}
           </li>
         ))}
       </ul>
